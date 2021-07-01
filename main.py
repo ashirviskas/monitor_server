1e03d1a98d2601700f8c1bd580fd01a20021c2d4
Matas Minelga <minematas@gmail.com>
2021-07-02 00:23:48 +0300

    First working iteration

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-import pyautogui
+from screenshotter import ScreenShotter
+from flask import Flask, render_template
 
+ss = ScreenShotter()
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route("/")
+def screenshot():
+    ss.do_screenshot()
+    return render_template('main.html',)
+
+
+app.run()
--- screenshotter.py
+++ screenshotter.py
@@ -1,12 +1,16 @@
 import pyautogui
 import time
 
+
 class ScreenShotter():
     def __init__(self, interval=1):
         self.interval = interval
 
+    def do_screenshot(self):
+        screenshot = pyautogui.screenshot(region=(0, 0, 600, 720))
+        screenshot.save('./static/screenshot.png')
+
     def make_screenshots(self):
         while True:
-            screenshot = pyautogui.screenshot
-            screenshot.save('./imgs/screenshot.png')
+            self.do_screenshot()
             time.sleep(self.interval)
